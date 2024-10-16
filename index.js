bb0da4301363f38f0a10b73e709d9fc7bc4c4c57
Muhammad Zawwar Akram <156500678+IbtahajAkram@users.noreply.github.com>
2024-10-16 16:58:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require(cors());
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.get('/', (req,res)=>{
+console.log('Backend Deploy hogaya ha');
+res.json({message:'Backend Deploy hogaya ha'})
+});
+
+app.listen(port, ()=>{
+   console.log('Your port is running on 3000') 
+});
